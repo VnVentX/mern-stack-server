3a83f35b546a0429840fc02c04c52afd20c026ca
AnhTT <nagaishoutaro@gmail.com>
2019-12-04 18:21:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require('dotenv/config')
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
 
 //Middleware
-app.use(cors())
+app.use(cors());
 //JSON parser
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION,
-    { useNewUrlParser: true },
-    () => console.log('Connected to DB at port 4000')
-)
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log("Connected to DB at port 4000")
+);
 
 //Import routes
-const userRoute = require('./routes/user')
-app.use('/user', userRoute)
+const userRoute = require("./routes/user");
+app.use("/user", userRoute);
 
 //Start server
-app.listen("https://prm391-react-native.herokuapp.com/")
+app.listen(process.env.PORT || 4000);
